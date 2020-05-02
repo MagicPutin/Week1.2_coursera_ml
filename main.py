727c28dac6e2ce248cccde4f76b400f08aaafbf6
MagicPutin <Vanya0411>
2020-05-02 23:14:23 +0300

    finish week 1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 
 # Обратите внимание, что признак Sex имеет строковые значения.
 row_data = pd.read_csv('data/titanic.csv', index_col='PassengerId')
@@ -8,9 +9,9 @@ row_data = pd.read_csv('data/titanic.csv', index_col='PassengerId')
 # delete nan age
 row_data = row_data.dropna(subset=['Age'])
 
-#################################################
-# NEED to transfer female and male into 1 and 0 #
-#################################################
+# transfer sex to 0 or 1
+row_data['Sex'] = row_data['Sex'].transform(func=lambda x: 0 if (x == 'female') else 1)
+# guess it's work
 
 data = row_data[['Pclass', 'Fare', 'Age', 'Sex']]
 goal = row_data['Survived']
@@ -18,3 +19,6 @@ goal = row_data['Survived']
 clf = DecisionTreeClassifier()
 clf.fit(data, goal)
 print(clf.feature_importances_)
+task = open('answers/task.txt', 'w')
+task.write('Fare Sex')
+task.close()
